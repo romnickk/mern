f6d8f5bb721c5a6600ac25b63bf3dd167f2d7948
romnick <romnicksanjuan9@gmail.com>
2024-01-10 01:04:34 +0800

    aaa

--- client/App.jsx
+++ client/App.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './src/pages/home';
 import About from './src/pages/about';
-import Register from './src/pages/register';
+// import Register from './src/pages/register';
 import Display from './src/pages/display';
 import Login from './src/pages/login';
 import ProductList from './src/productPages/productList';
@@ -23,7 +23,7 @@ function App() {
       <Routes>
         <Route path='/api/home' element={<Home/>}></Route>
         <Route path='/api/about' element={<About/>}></Route>
-        <Route path='/' element={<Register/>}></Route>
+        <Route path='/' element={<Home/>}></Route>
         <Route path='/api/display' element={<Display/>}></Route>
         <Route path='/api/login' element={<Login/>}></Route>
 
